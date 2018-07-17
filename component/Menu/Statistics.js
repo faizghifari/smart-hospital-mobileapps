c4115abb4b8876fe1c1431e8609f43d22fca9328
Unknown <rezkyal2@gmail.com>
2018-07-17 11:45:53 +0800

    statistics layout done

--- component/Menu/Statistics.js
+++ component/Menu/Statistics.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'React'
-import {View,Text,StatusBar,ImageBackground,TouchableOpacity,StyleSheet,FlatList,TextInput,KeyboardAvoidingView} from 'react-native'
+import {View,Text,StatusBar,ImageBackground,TouchableOpacity,StyleSheet,FlatList,TextInput,KeyboardAvoidingView,Picker} from 'react-native'
 import {Button, DatePicker} from 'native-base'
 import Chart from './../Chart/Chart.js'
 import {MIcon as Icon} from './../Utilities/Icon.js';
@@ -112,6 +112,60 @@ export default class Statistics extends Component{
         desc:'10 Hospital'
       },
     ]
+    this.dataHospital=[
+      {
+        nama:'AAA',
+        desc:'Jalan abc - Johor'
+      },
+      {
+        nama:'BBB',
+        desc:'Jalan aab - Melacca'
+      },
+      {
+        nama:'CCC',
+        desc:'Jalan aba - KL'
+      },
+      {
+        nama:'DDD',
+        desc:'Jalan cba'
+      },
+      {
+        nama:'EAC',
+        desc:'Jalan casca'
+      },
+      {
+        nama:'Sarawak',
+        desc:'10 Hospital'
+      },
+      {
+        nama:'Perak',
+        desc:'10 Hospital'
+      },
+      {
+        nama:'Kedah',
+        desc:'10 Hospital'
+      },
+      {
+        nama:'Pahang',
+        desc:'10 Hospital'
+      },
+      {
+        nama:'Trengganu',
+        desc:'10 Hospital'
+      },
+      {
+        nama:'Kelantan',
+        desc:'10 Hospital'
+      },
+      {
+        nama:'Negeri Sembilan',
+        desc:'10 Hospital'
+      },
+      {
+        nama:'Perlis',
+        desc:'10 Hospital'
+      },
+    ]
     this.state = {
       main: 0,
       chosenDate: new Date(),
@@ -119,23 +173,41 @@ export default class Statistics extends Component{
       chosenDetail:'',
       searchText:null,
       isLoading:false,
-      dataSource: dataProvince
+      dataProvince: this.dataProvince,
+      dataHospital: this.dataHospital,
+      dateType:'daily',
+      month:'07',
+      year:'18'
     };
     this.setDate = this.setDate.bind(this);
-    this.arrayholder=dataProvince
+    this.arrayProvince=this.dataProvince;
+    this.arrayHospital=this.dataHospital;
   }
 
 
   SearchFilterFunction(text){
-    const newData = this.arrayholder.filter(function(item){
+    if(this.state.main==2){
+      var newData = this.arrayProvince
+    }else if(this.state.main==3){
+      var newData = this.arrayHospital
+    }
+    newData=newData.filter(function(item){
       const itemData = item.nama.toUpperCase()
       const textData = text.toUpperCase()
       return itemData.indexOf(textData) > -1
     })
-    this.setState({
-        dataSource: newData,
-        text: text
-    })
+    if(this.state.main==2){
+      this.setState({
+          dataProvince: newData,
+          text: text
+      })
+    }else if(this.state.main==3){
+      this.setState({
+          dataHospital: newData,
+          text: text
+      })
+    }
+
   }
 
   setDate(newDate) {
@@ -157,7 +229,7 @@ export default class Statistics extends Component{
     })
   }
 
-  listProvince(item){
+  listItem(item){
     return(
       <TouchableOpacity style={styles.selectButton} onPress={this.chooseDetail.bind(this,[4,item.nama])}>
         <View style={styles.textContainer}>
@@ -170,7 +242,8 @@ export default class Statistics extends Component{
 
   backButtonHandler(){
     this.setState({
-      dataSource=this.dataProvince
+      dataProvince:this.dataProvince,
+      dataHospital:this.dataHospital
     })
     if(this.state.main==0){
       this.props.changeMenu(0)
@@ -219,8 +292,8 @@ export default class Statistics extends Component{
         <View style={{flex:0.9,flexDirection:'column'}}>
           <FlatList
             style={{flex:1}}
-            data={this.state.dataSource}
-            renderItem={({item})=>this.listProvince(item)}
+            data={this.state.dataProvince}
+            renderItem={({item})=>this.listItem(item)}
             keyExtractor={(item,key) => key.toString()}
           />
         </View>
@@ -228,7 +301,14 @@ export default class Statistics extends Component{
       var title='Statistics - State/Province'
     }else if (this.state.main==3){
       var main=(
-        <StatisticsDetail data={data}/>
+        <View style={{flex:0.9,flexDirection:'column'}}>
+          <FlatList
+            style={{flex:1}}
+            data={this.state.dataHospital}
+            renderItem={({item})=>this.listItem(item)}
+            keyExtractor={(item,key) => key.toString()}
+          />
+        </View>
       )
       var title='Statistics - Hospital'
     }else if (this.state.main==4){
@@ -271,23 +351,80 @@ export default class Statistics extends Component{
         </KeyboardAvoidingView>
       )
     }else{
-      if(this.)
-      var pickerSelector=(
-        <DatePicker
-          defaultDate={new Date(2018, 4, 4)}
-          minimumDate={new Date(2018, 1, 1)}
-          maximumDate={new Date(2018, 12, 31)}
-          locale={"en"}
-          timeZoneOffsetInMinutes={undefined}
-          modalTransparent={false}
-          animationType={"fade"}
-          androidMode={"default"}
-          placeHolderText="Select date"
-          textStyle={{ color: "white" }}
-          placeHolderTextStyle={{ color: "white" }}
-          onDateChange={this.setDate}
-        />
-      )
+      if(this.state.dateType=='daily'){
+        var pickerSelector=(
+          <DatePicker
+            defaultDate={new Date(2018, 7, 17)}
+            minimumDate={new Date(2018, 1, 1)}
+            maximumDate={new Date(2018, 12, 31)}
+            locale={"en"}
+            timeZoneOffsetInMinutes={undefined}
+            modalTransparent={false}
+            animationType={"fade"}
+            androidMode={"default"}
+            placeHolderText="Select date"
+            textStyle={{ color: "white" }}
+            placeHolderTextStyle={{ color: "white" }}
+            onDateChange={this.setDate}
+          />
+        )
+      }else if (this.state.dateType=='monthly') {
+        var pickerSelector=(
+          <View>
+            <Picker
+              selectedValue={this.state.month}
+              style={{color:'white', height: 50, width:80 }}
+              onValueChange={(itemValue, itemIndex) => this.setState({month: itemValue})}>
+              <Picker.Item label="Jan" value="01" />
+              <Picker.Item label="Feb" value="02" />
+              <Picker.Item label="Mar" value="03" />
+              <Picker.Item label="Apr" value="04" />
+              <Picker.Item label="May" value="05" />
+              <Picker.Item label="Jun" value="06" />
+              <Picker.Item label="Jul" value="07" />
+              <Picker.Item label="Agst" value="08" />
+              <Picker.Item label="Sept" value="09" />
+              <Picker.Item label="Oct" value="10" />
+              <Picker.Item label="Nov" value="11" />
+              <Picker.Item label="Dec" value="12" />
+            </Picker>
+            <Picker
+              selectedValue={this.state.year}
+              style={{color:'white', height: 30, width:80 }}
+              onValueChange={(itemValue, itemIndex) => this.setState({year: itemValue})}>
+              <Picker.Item label="2010" value="18" />
+              <Picker.Item label="2011" value="19" />
+              <Picker.Item label="2012" value="20" />
+              <Picker.Item label="2013" value="21" />
+              <Picker.Item label="2014" value="22" />
+              <Picker.Item label="2015" value="23" />
+              <Picker.Item label="2016" value="24" />
+              <Picker.Item label="2017" value="25" />
+              <Picker.Item label="2018" value="26" />
+            </Picker>
+          </View>
+        )
+      }else{
+        var pickerSelector=(
+          <View>
+            <Picker
+              selectedValue={this.state.year}
+              style={{color:'white', height: 50, width:80 }}
+              onValueChange={(itemValue, itemIndex) => this.setState({year: itemValue})}>
+              <Picker.Item label="2010" value="18" />
+              <Picker.Item label="2011" value="19" />
+              <Picker.Item label="2012" value="20" />
+              <Picker.Item label="2013" value="21" />
+              <Picker.Item label="2014" value="22" />
+              <Picker.Item label="2015" value="23" />
+              <Picker.Item label="2016" value="24" />
+              <Picker.Item label="2017" value="25" />
+              <Picker.Item label="2018" value="26" />
+            </Picker>
+          </View>
+        )
+
+      }
       var header=(
         <View style={{flex:0.1, flexDirection:'row', justifyContent:'center'}}>
           <View style={{flex:0.15, justifyContent:'center'}}>
@@ -295,8 +432,18 @@ export default class Statistics extends Component{
               <Icon name="arrow-back" style={{color: 'white'}}/>
             </Button>
           </View>
-          <View style={{flex:0.55, justifyContent:'center'}}>
+          <View style={{flex:0.35, justifyContent:'center'}}>
             <Text style={styles.titleFont}>{title}</Text>
+          </View>
+          <View style={{flex:0.20,justifyContent:'center'}}>
+            <Picker
+              selectedValue={this.state.dateType}
+              style={{color:'white', height: 50, width:80 }}
+              onValueChange={(itemValue, itemIndex) => this.setState({dateType: itemValue})}>
+              <Picker.Item label="Daily" value="daily" />
+              <Picker.Item label="Monthly" value="monthly" />
+              <Picker.Item label="Yearly" value="yearly" />
+            </Picker>
           </View>
           <View style={{flex:0.30, justifyContent:'center'}}>
             {pickerSelector}
