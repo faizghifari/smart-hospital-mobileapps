22dcd08d553be21380df6c091577e620a4a143e2
Reyhan D Rahman <rahman.reyhan@yahoo.com>
2018-08-11 03:14:58 +0800

    Edited WO

--- src/components/Users/Engineer/WorkOrder.js
+++ src/components/Users/Engineer/WorkOrder.js
@@ -11,17 +11,18 @@ import {
     StatusBar,
     Image,
     FlatList,
-    ScrollView
+    ScrollView,
 } from 'react-native';
 
 import Gradient from 'react-native-linear-gradient';
+import moment from 'moment';
 
 var Form = t.form.Form;
 
 // here we are: define your domain model
-var Person = t.struct({
+var person = t.struct({
     WoNumber: t.String,
-    WoDate: t.Date,
+    WoDate: t.String,
     assetNo: t.Number,
     hospital: t.String,
     location: t.String,
@@ -32,17 +33,16 @@ var requestForm = t.struct({
     requestor: t.String,
     designation: t.String,
     phone: t.String,
-    date: t.Date,
+    date: t.String,
     details: t.String,
-    radicare: t.Boolean
 });
 
 var assesmentDetails = t.struct({
     respondedBy: t.String,
-    respondDate: t.Date,
+    respondDate: t.String,
     rootCause: t.String,
     verifiedBy: t.String,
-    verifiedDate: t.Date,
+    verifiedDate: t.String,
     designation: t.String
 });
 
@@ -65,7 +65,7 @@ var sparePart = t.struct({
 
 var woReceipt = t.struct({
     receivedBy: t.String,
-    date: t.Date,
+    date: t.String,
     woCat: t.String,
     woType: t.String,
     wgCode: t.String,
@@ -84,7 +84,7 @@ var completion = t.struct({
     qcUptime: t.String,
     dvoName: t.String,
     assetRequiredButNotAvailable: t.Boolean,
-    handoverDate: t.Date,
+    handoverDate: t.String,
     acceptedBy: t.String,
     designation: t.String
 });
@@ -100,8 +100,111 @@ var costDetails = t.struct({
 });
 
 
-var options = {
-   
+let options = {
+    fields: {
+        WoNumber: {
+            editable: false
+        },
+        assetNo: {
+            editable: false
+        },
+        hospital: {
+            editable: false
+        },
+        location: {
+            editable: false
+        },
+        requestor: {
+            editable: false
+        },
+        phone: {
+            editable: false
+        },
+        respondedBy: {
+            editable: false
+        },
+        receivedBy: {
+            editable: false
+        },
+        woCat: {
+            editable: false
+        },
+        woType: {
+            editable: false
+        },
+        taskCode: {
+            editable: false
+        },
+        wgCode: {
+            editable: false
+        },
+        acceptedBy: {
+            editable: false
+        },
+        resourceType: {
+            editable: false
+        },
+        PoNo: {
+            editable: false
+        },
+        PoValue: {
+            editable: false
+        },
+        PoValue: {
+            editable:false
+        },
+        contractorCode: {
+            editable:false
+        },
+        contractorName: {
+            editable:false
+        },
+        verifiedBy: {
+            editable: false
+        },
+        WoDate: {
+            editable: false,
+            label: 'Work Order Date',
+        },
+        date: {
+            label: 'Date',
+            editable: false,
+        },
+        respondDate: {
+            label: 'Respond Date',
+            editable: false,
+        },
+        verifiedDate: {
+            label: 'Verified Date',
+            editable: false,
+        },
+
+        startDate: {
+            label: 'Start Date',
+            mode: 'date',
+            config: {
+                format: (date) => moment(date).format('MMMM Do YYYY'),
+            },
+        },
+        endDate: {
+            label: 'End Date',
+            mode: 'date',
+            config: {
+                format: (date) => moment(date).format('MMMM Do YYYY'),
+            },
+        },
+        targetDate: {
+            label: 'Target Date',
+            mode: 'date',
+            config: {
+                format: (date) => moment(date).format('MMMM Do YYYY'),
+            },
+        },
+        handoverDate: {
+            label: 'Handover Date',
+            editable: false
+        },
+    },
 }; // optional rendering options (see documentation)
 
 //const stylesheet = _.cloneDeep(t.form.Form.stylesheet);
@@ -116,11 +219,165 @@ Form.stylesheet.buttonText.fontSize = 14;
 
 
 
-export default class workOrder extends Component {
+export default class WorkOrder extends Component {
+    componentDidMount() {
+        let person = {
+            WoNumber: '1130201',
+            WoDate: moment(new Date()).format('MMMM Do YYYY, h:mm:ss a').toString(),
+            assetNo: '123',
+            hospital: 'Sultanah Aminah',
+            location: 'Johor Bahru',
+            desc: null
+        }
+        let requestForm = {
+            requestor: "Rezky Alamsyah",
+            designation: null,
+            phone: "+60-2321-12312",
+            date: moment(new Date()).format('MMMM Do YYYY, h:mm:ss a').toString(),
+            details: null
+        }
+        let taskEmployee = {
+            taskCode: '1230923',
+            task: null,
+            startDate: null,
+            endDate: null,
+            employeeCode: null,
+            prepHours: null,
+            repHours: null,
+            verHours: null,
+        }
+        let woReceipt = {
+            receivedBy: 'Reyhan Danu Rahman',
+            date: moment(new Date()).format('MMMM Do YYYY, h:mm:ss a').toString(),
+            details: null,
+            woCat: 'Category',
+            woType: 'PPM',
+            wgCode: '23DFSDF32',
+            targetDate: null,
+            priority: null,
+            estHours: null,
+            parts: null,
+            cancelledBy: null,
+            reason: null,
+        }
+        let assesmentDetails = {
+            respondedBy: "Rezky Alamsyah",
+            respondDate: "August 9th 2018",
+            rootCause: null,
+            verifiedBy: "Reyhan Danu Rahman",
+            verifiedDate: moment(new Date()).format('MMMM Do YYYY, h:mm:ss a').toString(),
+            designation: null
+        }
+
+        let completion = {
+            actionTaken: null,
+            qcPPM: null,
+            qcUptime: null,
+            dvoName: null,
+            assetRequiredButNotAvailable: null,
+            handoverDate: moment(new Date()).format('MMMM Do YYYY, h:mm:ss a').toString(),
+            acceptedBy: "Reyhan Danu Rahman",
+            designation: null
+        }
+
+        let costDetails = {
+            resourceType: "Cardio",
+            PoNo: "2070911",
+            PoValue: 22,
+            contractorCode: "98093HF",
+            contractorName: "iLife",
+            misc: null,
+            remarks: null
+        }
+
+
+
+        this.setState({
+            person: person,
+            requestForm: requestForm,
+            assesmentDetails: assesmentDetails,
+            taskEmployee: taskEmployee,
+            woReceipt: woReceipt,
+            taskEmployee: taskEmployee,
+            completion: completion,
+            costDetails: costDetails
+        })
+    }
     constructor(props) {
         super(props);
         this.state = {
-            theState: 0
+            theState: 0,
+            person: {
+                WoNumber: null,
+                WoDate: null,
+                assetNo: null,
+                hospital: null,
+                location: null,
+                description: null,
+            },
+            requestForm: {
+                requestor: null,
+                designation: null,
+                phone: null,
+                date: null,
+                details: null,
+            },
+             woReceipt: {
+                receivedBy: null,
+                date: null,
+                woCat: null,
+                woType: null,
+                wgCode: null,
+                targetDate: null,
+                priority: null,
+                estHours: null,
+                parts: null,
+                cancelledBy: null,
+                reason: null,
+            },
+            assesmentDetails: {
+                respondedBy: null,
+                respondDate: null,
+                rootCause: null,
+                verifiedBy: null,
+                verifiedDate: null,
+                designation: null
+            },
+            taskEmployee: {
+                taskCode: null,
+                task: null,
+                startDate: null,
+                endDate: null,
+                employeeCode: null,
+                prepHours: null,
+                repHours: null,
+                verHours: null,
+            },
+            sparePart: {
+                partCode: null,
+                description: null,
+                quantity: null
+            },
+           
+            completion: {
+                actionTaken: null,
+                qcPPM: null,
+                qcUptime: null,
+                dvoName: null,
+                assetRequiredButNotAvailable: null,
+                handoverDate: null,
+                acceptedBy: null,
+                designation: null
+            },
+            costDetails: {
+                resourceType: null,
+                PoNo: null,
+                PoValue: null,
+                contractorCode: null,
+                contractorName: null,
+                misc: null,
+                remarks: null
+            }
         };
 
         this.onPress = this.onPress.bind(this);
@@ -130,15 +387,52 @@ export default class workOrder extends Component {
         // console.log("REFS.FORM");
         // console.log(this._formRef);
         var value = this._formRef.getValue();
-       if (value) { // if validation fails, value will be null
-            // console.log(value); // value here is an instance of Person
-            this.setState({
-                theState: i
-            });
+        if (value) { // if validation fails, value will be null
+            console.log(value); // value here is an instance of Person
+            if (i == 1) {
+                this.setState({
+                    person: value
+                })
+            } else if (i == 2) {
+                this.setState({
+                    requestForm: value
+                })
+            } else if (i == 3) {
+                this.setState({
+                    woReceipt: value
+                })
+            } else if (i == 4) {
+                this.setState({
+                    assesmentDetails: value
+                })
+            } else if (i == 5) {
+                this.setState({
+                    taskEmployee: value
+                })
+            } else if (i == 6) {
+                this.setState({
+                    sparePart: value
+                })
+            } else if (i == 7) {
+                this.setState({
+                    completion: value
+                })
+            } else if (i == 8) {
+                this.setState({
+                    costDetails: value
+                })
+            }
+            if (i != 8) {
+                this.setState({
+                    theState: i
+                });
+            } else {
+                this.props.changeStepMaintenance(2)
+            }
         }
     }
-
     render() {
+        console.log(this.state.assesmentDetails)
         if (this.state.theState == 0) {
             var main = (
                 <ScrollView style={styles.container}>
@@ -146,10 +440,11 @@ export default class workOrder extends Component {
                     <Text style={{ fontWeight: 'bold', fontSize: 18, color: 'black', textAlign: 'center', marginBottom: 20 }}>WORK ORDER</Text>
                     <Form
                         ref={(ref) => this._formRef = ref}
-                        type={Person}
+                        type={person}
                         options={options}
+                        value={this.state.person}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,1)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 1)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Next</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -165,8 +460,9 @@ export default class workOrder extends Component {
                         ref={(ref) => this._formRef = ref}
                         type={requestForm}
                         options={options}
+                        value={this.state.requestForm}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,2)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 2)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Next</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -182,8 +478,9 @@ export default class workOrder extends Component {
                         ref={(ref) => this._formRef = ref}
                         type={woReceipt}
                         options={options}
+                        value={this.state.woReceipt}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,3)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 3)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Next</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -199,8 +496,9 @@ export default class workOrder extends Component {
                         ref={(ref) => this._formRef = ref}
                         type={assesmentDetails}
                         options={options}
+                        value={this.state.assesmentDetails}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,4)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 4)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Next</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -216,8 +514,9 @@ export default class workOrder extends Component {
                         ref={(ref) => this._formRef = ref}
                         type={taskEmployee}
                         options={options}
+                        value={this.state.taskEmployee}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,5)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 5)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Next</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -234,7 +533,7 @@ export default class workOrder extends Component {
                         type={sparePart}
                         options={options}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,6)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 6)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Next</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -250,8 +549,9 @@ export default class workOrder extends Component {
                         ref={(ref) => this._formRef = ref}
                         type={completion}
                         options={options}
+                        value={this.state.completion}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,7)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 7)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Next</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -267,8 +567,9 @@ export default class workOrder extends Component {
                         ref={(ref) => this._formRef = ref}
                         type={costDetails}
                         options={options}
+                        value={this.state.costDetails}
                     />
-                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this,0)} underlayColor='#99d9f4'>
+                    <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this, 8)} underlayColor='#99d9f4'>
                         <Text style={styles.buttonText}>Submit</Text>
                     </TouchableHighlight>
                 </ScrollView>
@@ -276,15 +577,16 @@ export default class workOrder extends Component {
         }
 
         return (
-            <Gradient 
+            <Gradient
                 colors={['#FFFFFF', '#FFEFBA']}
                 style={{ flex: 1, flexDirection: 'column' }}>
-                
-                    {main}
-      
-                </Gradient>
-            
-
+                <StatusBar
+                    backgroundColor="#FFFFFF"
+                    animated={true}
+                    barStyle='light-content'
+                />
+                {main}
+            </Gradient>
         );
     }
 }
